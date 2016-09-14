64b85b64aade7484249a7c79f83b994a74484e8b
Wes Cruver <chieffancypants@gmail.com>
2016-09-13 18:01:47 -0700

    lots of cleanup

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-// var LocalStorage = require('node-localstorage').LocalStorage;
-// var localStorage = new LocalStorage('./tmp')
 
+const debug = require('debug')('FastMutex');
 
-function busyLoop (ms) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > ms) {
-      break;
-    }
-  }
-}
+/**
+ * Helper function to create a randomId to distinguish between different
+ * FastMutex clients.  localStorage uses strings, so explicitly cast to string:
+ */
+const randomId = () => Math.random() + '';
 
-function randomId () {
-  return Math.random() * 1000000000 + '';
-}
-
-function resolveWithStats (resolve, stats) {
+/**
+ * Helper function to calculate the endTime, lock acquisition time, and then
+ * resolve the promise with all the lock stats
+ */
+const resolveWithStats = (resolve, stats) => {
   stats.endTime = new Date().getTime();
   stats.timeToAcquire = stats.endTime - stats.startTime;
   resolve(stats);
 }
 
+
+// TODO: Rename ID to clientId
 class FastMutex {
-  constructor ({ localStorageKey, id = randomId(), x = '_MUTEX_LOCK_X', y = '_MUTEX_LOCK_Y', localStorage }) {
-    this.lsKey = localStorageKey
+  constructor ({ id = randomId(), xPrefix = '_MUTEX_LOCK_X_', yPrefix = '_MUTEX_LOCK_Y_', timeout = 5000, localStorage }) {
     this.id = id
-    this.x = `${x}_${localStorageKey}`;
-    this.y = `${y}_${localStorageKey}`;
+    this.xPrefix = xPrefix;
+    this.yPrefix = yPrefix;
+    this.timeout = timeout;
+
     this.localStorage = localStorage || window.localStorage
     this.lockStats = {
       restartCount: 0,
@@ -37,81 +36,94 @@ class FastMutex {
     };
   }
 
-  // TODO: should record number of runs, locks, contentions, etc and return in stats property of promise resolution
-  lock (key, id, localStorage) {
-    localStorage = localStorage || this.localStorage;
-    id = id || 'randomId';
+  lock (key) {
+    debug('Attempting to acquire Lock on "%s" using FastMutex instance "%s"', key, this.id)
+    const x = this.xPrefix + key;
+    const y = this.yPrefix + key;
 
     if (!this.lockStats.startTime) {
       this.lockStats.startTime = new Date().getTime();
     }
 
     return new Promise((resolve, reject) => {
-      let x = `mutexlock_x_${key}`;
-      let y = `mutexlock_y_${key}`;
+      const elapsedTime = new Date().getTime() - this.lockStats.startTime;
+      if (elapsedTime >= this.timeout) {
+        debug('Lock on "%s" could not be acquired by FastMutex client "%s"', key, this.id);
+        return reject(`Lock could not be acquired within ${this.timeout}ms`);
+      }
 
-      // console.log('acquireLock', key, localStorage.getItem(x));
-      localStorage.setItem(x, id);
+      this.localStorage.setItem(x, this.id);
 
       // if y exists, another client is getting a lock, so retry in a bit
-      let lsY = localStorage.getItem(y);
+      let lsY = this.localStorage.getItem(y);
       if (lsY) {
-        console.error('locked.  restarting...', lsY);
+        debug('Lock exists on Y (%s), restarting...', lsY);
         this.lockStats.restartCount++;
-        setTimeout(() => this.lock(key, id, localStorage).then(resolve));
+        setTimeout(() => this.lock(key).then(resolve).catch(reject));
         return;
       }
 
       // ask for inner lock
-      localStorage.setItem(y, id);
+      this.localStorage.setItem(y, this.id);
 
       // if x was changed, another client is contending for an inner lock
-      let lsX = localStorage.getItem(x);
-      if (lsX !== id) {
+      let lsX = this.localStorage.getItem(x);
+      if (lsX !== this.id) {
         this.lockStats.contentionCount++;
-        console.log('lsx !== id', lsX, id);
+        debug('Lock contention detected. X="%s"', lsX);
+
         // Give enough time for critical section:
         setTimeout(() => {
-          if (localStorage.getItem(y) !== id) {
+          lsY = this.localStorage.getItem(y);
+          if (lsY !== this.id) {
+            // we lost the lock, restart the process again
             this.lockStats.restartCount++;
             this.lockStats.locksLost++;
-            console.log('lock was lost, restarting...');
-            // we lost the lock, restart the process again
-            setTimeout(() => this.lock(key, id, localStorage).then(resolve));
+            debug('FastMutex client "%s" lost the lock contention on "%s" to another process (%s). Restarting...', this.id, key, lsY)
+            setTimeout(() => this.lock(key).then(resolve).catch(reject));
             return;
           } else {
             // we have a lock
+            debug('FastMutex client "%s" won the lock contention on "%s"', this.id, key);
             resolveWithStats(resolve, this.lockStats)
-            console.log('we gots a lock');
           }
         }, 50);
         return;
       }
 
       // no contention:
-      console.log('aquired lock without contention');
+      debug('FastMutex client "%s" acquired a lock on "%s" with no contention', this.id, key);
       resolveWithStats(resolve, this.lockStats);
     })
   }
 
   release (key) {
-    let y = `mutexlock_y_${key}`;
+    debug('FastMutex client "%s" is releasing lock on "%s"', this.id, key);
+    const y = this.yPrefix + key;
     return new Promise((resolve, reject) => {
-      return this.localStorage.setItem(key, 0);
+      this.localStorage.removeItem(y);
+      resolve();
     });
+  }
+
+  /**
+   * Helper function to wrap all values in an object that includes the time (so
+   * that we can expire it in the future) and json.stringify's it
+   */
+  setItem (key, value) {
+    return this.localStorage.setItem(key, JSON.stringify({
+      expiresAt: new Date().getTime() + this.timeout,
+      value
+    }));
+  }
+
+  /**
+   * Helper function to parse JSON encoded values set in localStorage
+   */
+  getItem (key) {
+    return JSON.parse(this.localStorage.getItem(key));
   }
 
 }
 
 module.exports = FastMutex;
-
-// setTimeout(() => {
-//   document.body.innerHTML = 'BUSYLOOP';
-//   busyLoop(1000);
-//   document.body.innerHTML = 'BUSYLOOP2';
-//
-//   var syncedVal = new FastMutex({ localStorageKey: 'clientId' });
-//   syncedVal.sync(Math.ceil(randomId()));
-//   // var body = document.body.innerHTML = localStorage.getItem('clientId')
-//   var body = document.body.innerHTML = syncedVal.id;
-// });
--- test.js
+++ test.js
@@ -20,13 +20,12 @@ describe('FastMutex', () => {
     sandbox.restore();
     let len = localStorage.length;
     localStorage.clear();
-    console.log('localStorage cleared');
     expect(localStorage.length).to.equal(0);
   });
 
   it('should immediately establish a lock when there is no contention', () => {
-    var fm1 = new FastMutex({ localStorageKey: 'test', localStorage: localStorage });
-    var fm2 = new FastMutex({ localStorageKey: 'test', localStorage: localStorage });
+    var fm1 = new FastMutex({ localStorage: localStorage });
+    // var fm2 = new FastMutex({ localStorage: localStorage });
 
     return fm1.lock('clientId').then((stats) => {
       expect(stats.restartCount).to.be.equal(0);
@@ -36,14 +35,14 @@ describe('FastMutex', () => {
   });
 
   it('When another client has a lock (Y is not 0), it should restart to acquire a lock at a later time', function () {
-    var fm1 = new FastMutex({ localStorageKey: 'test', localStorage: localStorage });
+    var fm1 = new FastMutex({ xPrefix: 'xPrefix_', yPrefix: 'yPrefix_', localStorage: localStorage });
     let spy = sandbox.spy(fm1, 'lock');
+
     const key = 'clientId';
-    localStorage.setItem(`mutexlock_y_${key}`, 'abcd');
+    localStorage.setItem(`yPrefix_${key}`, 'someOtherMutexId');
 
     setTimeout(() => {
-      console.log('reset Y');
-      localStorage.removeItem(`mutexlock_y_${key}`);
+      localStorage.removeItem(`yPrefix_${key}`);
     }, 10);
 
     return fm1.lock(key).then((stats) => {
@@ -57,7 +56,8 @@ describe('FastMutex', () => {
 
   it('when contending for a lock and ultimately losing, it should restart', () => {
     const key = 'clientId';
-    var stub = sandbox.stub(localStorage, 'getItem')
+    const fm = new FastMutex({ localStorage: localStorage, id: 'uniqueId' });
+    const stub = sandbox.stub(localStorage, 'getItem')
 
     // Set up scenario for lock contention where we lost Y
     stub.onCall(0).returns(null)  // getItem Y
@@ -66,10 +66,9 @@ describe('FastMutex', () => {
 
     // fastmutex should have restarted, so let's free up the lock:
     stub.onCall(3).returns(null)
-    stub.onCall(4).returns('randomId')
+    stub.onCall(4).returns('uniqueId')
 
-    const fm = new FastMutex({ localStorageKey: 'test', localStorage: localStorage, id: 'uniqueId' });
-    var spy = sandbox.spy(fm, 'lock');
+    const spy = sandbox.spy(fm, 'lock');
 
     return fm.lock(key).then((stats) => {
       expect(stats.restartCount).to.be.equal(1);
@@ -80,17 +79,17 @@ describe('FastMutex', () => {
     });
   });
 
-  it.only('When contending for a lock and ultimately winning, it should not restart', () => {
+  it('When contending for a lock and ultimately winning, it should not restart', () => {
     const key = 'clientId';
-    var stub = sandbox.stub(localStorage, 'getItem')
+    const fm = new FastMutex({ localStorage: localStorage, id: 'uniqueId' });
+    const stub = sandbox.stub(localStorage, 'getItem')
 
     // Set up scenario for lock contention where we lost Y
     stub.onCall(0).returns(null)  // getItem Y
     stub.onCall(1).returns('lockContention');
-    stub.onCall(2).returns('randomId');
+    stub.onCall(2).returns('uniqueId');
 
-    const fm = new FastMutex({ localStorageKey: 'test', localStorage: localStorage, id: 'uniqueId' });
-    var spy = sandbox.spy(fm, 'lock');
+    const spy = sandbox.spy(fm, 'lock');
 
     return fm.lock(key).then((stats) => {
       expect(stats.restartCount).to.be.equal(0);
@@ -101,4 +100,113 @@ describe('FastMutex', () => {
     });
   });
 
+  // This is just to ensure that the internals of FastMutex have prefixes on the
+  // X and Y locks such that two different FM clients can acquire locks on
+  // different keys concurrently without clashing.
+  it('should not clash with other fastMutex locks', function () {
+    const yPrefix = 'yLock';
+    const xPrefix = 'xLock';
+    const opts = { localStorage, yPrefix, xPrefix };
+
+    const fm1 = new FastMutex(opts);
+    const fm2 = new FastMutex(opts);
+
+    let lock1Acquired = false;
+    let lock2Acquired = false;
+
+    const lock1Promise = fm1.lock('lock1').then((stats) => {
+      lock1Acquired = true;
+      expect(localStorage.getItem(yPrefix + 'lock1')).to.not.be.null;
+      return stats;
+    });
+
+    const lock2Promise = fm2.lock('lock2').then((stats) => {
+      lock2Acquired = true;
+      expect(localStorage.getItem(yPrefix + 'lock2')).to.not.be.null;
+      return stats
+    });
+
+    return Promise.all([lock1Promise, lock2Promise])
+      .then((stats) => {
+        expect(lock1Acquired).to.be.true;
+        expect(lock2Acquired).to.be.true;
+        expect(stats[0].restartCount).to.be.equal(0);
+        expect(stats[1].restartCount).to.be.equal(0);
+      });
+  });
+
+  it('release() should remove the y lock in localStorage', () => {
+    const key = 'clientId';
+    const fm1 = new FastMutex({ localStorage: localStorage, id: 'releaseTestId', yPrefix: 'yLock' });
+    return fm1.lock(key).then(() => {
+      expect(localStorage.getItem('yLock' + key)).to.be.equal('releaseTestId');
+      return fm1.release(key);
+    }).then(() => {
+      expect(localStorage.getItem('yLock' + key)).to.be.null;
+    })
+  });
+
+  // this is essentially just a better way to test that two locks cannot get
+  // an exclusive lock until the other releases.  It's a bit more accurate
+  // than the test above ("release should remove the y lock in localstorage")
+  it('two clients should never get locks at the same time', function () {
+    const fm1 = new FastMutex({ localStorage: localStorage });
+    const fm2 = new FastMutex({ localStorage: localStorage });
+    let fm1LockReleased = false;
+    const lockHoldTime = 10;
+
+    return fm1.lock('clientId').then(() => {
+      // before the lock is released, try to establish another lock:
+      var lock2Promise = fm2.lock('clientId');
+      expect(fm1LockReleased).to.be.false;
+
+      // in a few milliseconds, release the lock
+      setTimeout(() => {
+        fm1.release('clientId').then(() => fm1LockReleased = true)
+      }, lockHoldTime);
+
+      return lock2Promise;
+    }).then((lock2) => {
+      // this will only execute once the other lock was released
+      expect(fm1LockReleased).to.be.true;
+      expect(lock2.restartCount).to.be.above(1);
+      expect(lock2.timeToAcquire).to.be.above(lockHoldTime);
+    });
+  });
+
+  it('should throw if lock is never acquired after set time period', function () {
+    this.timeout(1000);
+    this.slow(500);
+
+    const fm1 = new FastMutex({ localStorage: localStorage, timeout: 50 });
+    const fm2 = new FastMutex({ localStorage: localStorage, timeout: 50 });
+
+    const p = fm1.lock('timeoutTest').then(() => {
+      // fm2 will never get a lock as we're not releasing fm1's lock:
+      return fm2.lock('timeoutTest')
+    });
+
+    return expect(p).to.eventually.be.rejected;
+  });
+
+  it.only('should release the lock automatically after set time period', (done) => {
+    const fm1 = new FastMutex({ localStorage: localStorage, timeout: 50, yPrefix: 'yLock', id: 'timeoutClient' });
+    const fm2 = new FastMutex({ localStorage: localStorage, timeout: 50, yPrefix: 'yLock', id: 'timeoutClient2' });
+
+    fm1.lock('timeoutTest').then(() => {
+      expect(localStorage.getItem('yLocktimeoutTest')).to.be.equal('timeoutClient');
+    });
+
+    setTimeout(() => {
+      fm2.lock('timeoutTest').then(() => {
+        expect(localStorage.getItem('yLocktimeoutTest')).to.be.equal('timeoutClient2');
+        done();
+      })
+    }, 75);
+    // const fm2 = new FastMutex({ localStorage: localStorage, timeout: 50 });
+  });
+
+  it.skip('should not allow a client to lock the same object twice', () => {
+
+  });
 });
